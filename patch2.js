0b4f4e19848fe7812d100b4d05b2f25d279d8919
laurentiucalonfir <laurentiucalonfir@gmail.com>
2026-08-23 13:03:14 +0300

    Increase font size of location and product dropdowns by 2px

--- patch2.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let js = fs.readFileSync('app.js', 'utf8');
-
-js = js.replace(/textDoarProduse = textDoarProduse.trim\\(\\);/, 'textDoarProduse = textDoarProduse.replace(/? /g, \\'\\').replace(/\\\\\*/g, \\'\\').replace(/?? /g, \\'\\').trim();');
-
-fs.writeFileSync('app.js', js);
